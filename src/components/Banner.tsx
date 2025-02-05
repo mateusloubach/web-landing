95bdcaef572139bf1bd86ea9a104af3aa7f161ab
mateusloubach <mcesarloubach@gmail.com>
2025-02-05 13:42:12 -0300

    docs: init project

---
+++ src/components/Banner.tsx
@@ -0,0 +1,9 @@
+export const Banner = () => {
+  return (
+    <div className="bg-gray-800 text-white text-center py-4">
+      <div className="container mx-auto">
+        <a className="underline underline-offset-4">Get 10% off your first order with code FIRSTORDER</a>
+      </div>
+    </div>
+  );
+};
